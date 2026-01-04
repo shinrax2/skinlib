f109a93f8718104c5bca19749abc5a842e7ddaaf
shinrax2 <shinra.ubu1@yahoo.de>
2026-01-04 15:02:02 +0100

    add example mod, textures by dribbleondo

---
+++ examples/timelines_skinlib/core.lua
@@ -0,0 +1,170 @@
+local part_body = {
+    part_id = "wpn_fps_pis_welrod_body_standard_timelines",
+    weapon_factory_id = "wpn_fps_pis_welrod",
+    base_part = "wpn_fps_pis_welrod_body_standard",
+    fps = {
+        wpn_fps_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    tps = {
+        wpn_third_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    materials = {
+        fps = {"wpn_fps_pis_welrod"},
+        tps = {"wpn_third_pis_welrod"},
+    },
+}
+
+local part_bolt = {
+    part_id = "wpn_fps_pis_welrod_bolt_standard_timelines",
+    weapon_factory_id = "wpn_fps_pis_welrod",
+    base_part = "wpn_fps_pis_welrod_bolt_standard",
+    fps = {
+        wpn_fps_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    tps = {
+        wpn_third_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    materials = {
+        fps = {"wpn_fps_pis_welrod"},
+        tps = {"wpn_third_pis_welrod"},
+    },
+}
+
+local part_m = {
+    part_id = "wpn_fps_pis_welrod_m_standard_timelines",
+    weapon_factory_id = "wpn_fps_pis_welrod",
+    base_part = "wpn_fps_pis_welrod_m_standard",
+    fps = {
+        wpn_fps_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    tps = {
+        wpn_third_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    materials = {
+        fps = {"wpn_fps_pis_welrod"},
+        tps = {"wpn_third_pis_welrod"},
+    },
+}
+
+local part_catch = {
+    part_id = "wpn_fps_pis_welrod_catch_timelines",
+    weapon_factory_id = "wpn_fps_pis_welrod",
+    base_part = "wpn_fps_pis_welrod_catch",
+    fps = {
+        wpn_fps_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    tps = {
+        wpn_third_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    materials = {
+        fps = {"wpn_fps_pis_welrod"},
+        tps = {"wpn_third_pis_welrod"},
+    },
+}
+
+local part_trigger = {
+    part_id = "wpn_fps_pis_welrod_trigger_timelines",
+    weapon_factory_id = "wpn_fps_pis_welrod",
+    base_part = "wpn_fps_pis_welrod_trigger",
+    fps = {
+        wpn_fps_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    tps = {
+        wpn_third_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    materials = {
+        fps = {"wpn_fps_pis_welrod"},
+        tps = {"wpn_third_pis_welrod"},
+    },
+}
+
+local part_safety = {
+    part_id = "wpn_fps_pis_welrod_safety_timelines",
+    weapon_factory_id = "wpn_fps_pis_welrod",
+    base_part = "wpn_fps_pis_welrod_safety",
+    fps = {
+        wpn_fps_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    tps = {
+        wpn_third_pis_welrod = {
+            bump_normal_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/wpn_fps_pis_welrod_nm",
+            diffuse_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_df",
+            material_texture = "units/upd_017/weapons/wpn_fps_pis_welrod_pts/textures/timelines_welrod_gsm"
+        }
+    },
+    materials = {
+        fps = {"wpn_fps_pis_welrod"},
+        tps = {"wpn_third_pis_welrod"},
+    },
+}
+
+local skin = {
+    skin_id = "sx2_ws_welrod_timelines",
+    name_id = "sx2_ws_welrod_timelines",
+    weapon_id = "welrod",
+    weapon_desc_id = "sx2_ws_welrod_timelines_desc",
+    weapon_name_id = "sx2_ws_welrod_timelines_name",
+    replaces_parts = {
+        wpn_fps_pis_welrod_body_standard = "wpn_fps_pis_welrod_body_standard_timelines",
+        wpn_fps_pis_welrod_bolt_standard = "wpn_fps_pis_welrod_bolt_standard_timelines",
+        wpn_fps_pis_welrod_m_standard = "wpn_fps_pis_welrod_m_standard_timelines",
+        wpn_fps_pis_welrod_catch = "wpn_fps_pis_welrod_catch_timelines",
+        wpn_fps_pis_welrod_trigger = "wpn_fps_pis_welrod_trigger_timelines",
+        wpn_fps_pis_welrod_safety = "wpn_fps_pis_welrod_safety_timelines",
+    },
+    gold_price = 1
+}
+
+if SkinLib then
+    SkinLib.AddSkinPart(part_body)
+    SkinLib.AddSkinPart(part_bolt)
+    SkinLib.AddSkinPart(part_m)
+    SkinLib.AddSkinPart(part_catch)
+    SkinLib.AddSkinPart(part_trigger)
+    SkinLib.AddSkinPart(part_safety)
+    SkinLib.AddSkin(skin)
+end
