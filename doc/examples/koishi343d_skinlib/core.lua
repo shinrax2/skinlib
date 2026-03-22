a4ff9cb0c4049ec10126a0a66fb9ccf8a20b6397
shinrax2 <shinra.ubu1@yahoo.de>
2026-03-22 17:40:59 +0100

    add koishi example

---
+++ doc/examples/koishi343d_skinlib/core.lua
@@ -0,0 +1,19 @@
+Hooks:Add(
+    "SkinLibCollectData",
+    "Koishi343d_skinlib.SkinLibCollectData",
+    function(self)
+        local skin = SL_Skin:new("sx2_ws_d343_koishi", "d343")
+        :set_name_id("sx2_ws_d343_koishi")
+        :set_weapon_name_id("sx2_ws_d343_koishi_name")
+        :set_weapon_desc_id("sx2_ws_d343_koishi_desc")
+        :set_gold_price(1)
+        :set_replaces_units({
+            unit = "units/upd_003/weapons/wpn_fps_gre_d343/wpn_fps_gre_d343_koishi",
+            unit_dummy = "units/upd_003/weapons/wpn_fps_gre_d343/wpn_fps_gre_d343_husk_koishi",
+            unit_hand = "units/upd_003/weapons/wpn_fps_gre_d343/wpn_fps_gre_d343_hand_koishi",
+        })
+
+        self.RegisterSkin(skin)
+
+    end
+)
